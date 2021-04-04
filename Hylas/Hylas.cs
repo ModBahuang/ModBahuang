261d4fda2ed3c8414833c2e39ac1e828f4f8c6fa
lolligun <80769475+lolligun@users.noreply.github.com>
2021-04-04 16:33:15 +0800

    Add Hylas experimentally

---
+++ Hylas/Hylas.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Harmony;
+using System.IO;
+using MelonLoader;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace Hylas
+{
+
+#pragma warning disable 649
+    // ReSharper disable MemberCanBePrivate.Global
+    // ReSharper disable InconsistentNaming
+
+    /* Avoid serialize issue caused by unhollower */
+    internal struct Rect
+    {
+        public Vector2 position;
+        public Vector2 size;
+
+        public static implicit operator UnityEngine.Rect(Rect v)
+        {
+            return new UnityEngine.Rect(v.position, v.size);
+        }
+    }
+
+    internal struct Vector2
+    {
+        public float x;
+        public float y;
+
+        public static implicit operator UnityEngine.Vector2(Vector2 v)
+        {
+            return new UnityEngine.Vector2(v.x, v.y);
+        }
+    }
+    internal struct Vector4
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public static implicit operator UnityEngine.Vector4(Vector4 v)
+        {
+            return new UnityEngine.Vector4(v.x, v.y, v.z, v.w);
+        }
+    }
+
+    internal struct SpriteParam
+    {
+        public Rect rect; 
+        public Vector2 pivot; 
+        public float pixelsPerUnit; 
+        public uint extrude; 
+        public SpriteMeshType meshType; 
+        public Vector4 border;
+        public bool generateFallbackPhysicsShape;
+    }
+    // ReSharper restore MemberCanBePrivate.Global
+    // ReSharper restore UnassignedField.Global
+    // ReSharper restore InconsistentNaming
+#pragma warning restore 649
+
+
+    public class Hylas: MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            var load = typeof(ResMgr).GetMethods().First(m => !m.IsGenericMethod && m.Name == "Load");
+            var pre = typeof(Hylas).GetMethod(nameof(PreResLoad), BindingFlags.Public | BindingFlags.Static);
+            Harmony.Patch(load, new HarmonyMethod(pre));
+        }
+        
+        // ReSharper disable once InconsistentNaming
+        public static bool PreResLoad(ref UnityEngine.Object __result, string path)
+        {
+            MelonLogger.Msg(path);
+
+            const string templateObjectPath = "Game/Portrait/Woman/Body/101";
+            if (!path.IsPortrait()) return true;
+
+            var pp = path.ToPhysicalPath();
+            if (!pp.Exist()) return true;
+
+            // Can't use base method directly because of the missing of reverse path.
+            // FIXME: There is no guarantee `Load<T>` won't use `Load` internally
+            var orig = g.res.Load<GameObject>(templateObjectPath);
+
+            var template = UnityEngine.Object.Instantiate(orig).Cast<GameObject>();
+            var renderer = template.GetComponentInChildren<SpriteRenderer>();
+            renderer.LoadCustomSprite(pp);
+
+            __result = template;
+
+            return false;
+        }
+        
+    }
+
+    internal static class Ext
+    {
+        public static bool IsPortrait(this string path)
+        {
+            return path.StartsWith("Game/Portrait/");
+        }
+
+        public static string ToPhysicalPath(this string path)
+        {
+            return Path.Combine(MelonUtils.GameDirectory, "Mods", nameof(Hylas), path.Replace("Game/Portrait/", ""));
+        }
+
+        public static bool Exist(this string path)
+        {
+            return Directory.Exists(path);
+        }
+
+        public static void LoadCustomSprite(this SpriteRenderer renderer, string path)
+        {
+            var imagePath = Path.Combine(path, "image.png");
+            var paramPath = Path.Combine(path, "sprite.json");
+            
+            var param = JsonConvert.DeserializeObject<SpriteParam>(File.ReadAllText(paramPath));
+            
+            var image = File.ReadAllBytes(imagePath);
+
+            // LoadImage will replace with with incoming image size.
+            var tex = new Texture2D(100, 100);
+
+            if (!ImageConversion.LoadImage(tex, image))
+            {
+                throw new InvalidOperationException();
+            }
+
+            var newSprite = Sprite.Create(tex, param.rect, param.pivot, param.pixelsPerUnit, param.extrude, param.meshType, param.border, param.generateFallbackPhysicsShape);
+
+            renderer.sprite = newSprite;
+        }
+    }
+}
---
+++ Hylas/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hylas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Hylas")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b76c1627-621e-46f8-af77-4a4615dfccfd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+
+[assembly: MelonInfo(typeof(Hylas.Hylas), "Hylas", "0.0.1", "A")]
+[assembly: MelonColor]
+
+[assembly: MelonGame]
