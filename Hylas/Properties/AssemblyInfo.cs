9a41ba0be63fd158b7485cfbeb6c6dcce73cc2b9
lolligun <80769475+lolligun@users.noreply.github.com>
2021-04-10 14:41:34 +0800

    Bump to 0.3.2

--- Hylas/Properties/AssemblyInfo.cs
+++ Hylas/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.1")]
-[assembly: AssemblyFileVersion("0.0.3.1")]
+[assembly: AssemblyVersion("0.0.3.2")]
+[assembly: AssemblyFileVersion("0.0.3.2")]
 
-[assembly: MelonInfo(typeof(Hylas.Hylas), "Hylas", "0.3.1", "A")]
+[assembly: MelonInfo(typeof(Hylas.Hylas), "Hylas", "0.3.2", "A")]
 [assembly: MelonColor]
 
 [assembly: MelonGame]
